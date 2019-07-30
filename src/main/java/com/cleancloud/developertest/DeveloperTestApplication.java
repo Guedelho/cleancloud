24d54b6b140a0bd8b2586a15d0a80d4d759645a7
Guedelho <mateusguedelho@yahoo.com.br>
2019-07-30 00:19:45 -0300

    google cloud sdk connected

--- src/main/java/com/cleancloud/developertest/DeveloperTestApplication.java
+++ src/main/java/com/cleancloud/developertest/DeveloperTestApplication.java
@@ -1,5 +1,7 @@
 package com.cleancloud.developertest;
 
+import com.google.cloud.bigquery.*;
+import com.google.cloud.bigquery.QueryResponse;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,18 +16,64 @@ import java.util.Set;
 @SpringBootApplication
 public class DeveloperTestApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(DeveloperTestApplication.class, args);
+
+        // [START bigquery_simple_app_client]
+        BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+        // [END bigquery_simple_app_client]
+        // [START bigquery_simple_app_query]
+        QueryJobConfiguration queryConfig =
+                QueryJobConfiguration.newBuilder(
+                        "SELECT "
+                                + "CONCAT('https://stackoverflow.com/questions/', CAST(id as STRING)) as url, "
+                                + "view_count "
+                                + "FROM `bigquery-public-data.stackoverflow.posts_questions` "
+                                + "WHERE tags like '%google-bigquery%' "
+                                + "ORDER BY favorite_count DESC LIMIT 10")
+                        // Use standard SQL syntax for queries.
+                        // See: https://cloud.google.com/bigquery/sql-reference/
+                        .setUseLegacySql(false)
+                        .build();
+
+        // Create a job ID so that we can safely retry.
+        JobId jobId = JobId.of("asdf");
+        Job queryJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+
+        // Wait for the query to complete.
+        queryJob = queryJob.waitFor();
+
+        // Check for errors
+        if (queryJob == null) {
+            throw new RuntimeException("Job no longer exists");
+        } else if (queryJob.getStatus().getError() != null) {
+            // You can also look at queryJob.getStatus().getExecutionErrors() for all
+            // errors, not just the latest one.
+            throw new RuntimeException(queryJob.getStatus().getError().toString());
+        }
+        // [END bigquery_simple_app_query]
+
+        // [START bigquery_simple_app_print]
+        // Get the results.
+        TableResult result = queryJob.getQueryResults();
+
+        // Print all pages of the results.
+        for (FieldValueList row : result.iterateAll()) {
+            String url = row.get("url").getStringValue();
+            long viewCount = row.get("view_count").getLongValue();
+            System.out.printf("url: %s views: %d%n", url, viewCount);
+        }
+        // [END bigquery_simple_app_print]
     }
 
-    @Bean
-    DynamoDbClient dynamoDbClient() {
-        return DynamoDbClient.create();
-    }
-
-    @Bean
-    ApplicationRunner applicationRunner(DynamoDbClient dynamoDbClient) {
-        return args -> {
+//    @Bean
+//    DynamoDbClient dynamoDbClient() {
+//        return DynamoDbClient.create();
+//    }
+//
+//    @Bean
+//    ApplicationRunner applicationRunner(DynamoDbClient dynamoDbClient) {
+//        return args -> {
 //			HashMap<String,AttributeValue> item_values =
 //					new HashMap<String,AttributeValue>();
 //
@@ -40,21 +88,20 @@ public class DeveloperTestApplication {
 //					.build();
 //			DynamoDbClient ddb = DynamoDbClient.create();
 //			ddb.putItem(request);
-
-            HashMap<String, AttributeValue> key_to_get =
-                    new HashMap<String, AttributeValue>();
-
-            key_to_get.put("name", AttributeValue.builder()
-                    .s("name").build());
-
-            DeleteItemRequest request = DeleteItemRequest.builder()
-                    .key(key_to_get)
-                    .tableName("mateusserejo_github")
-                    .build();
-
-            DynamoDbClient ddb = DynamoDbClient.create();
-            ddb.deleteItem(request);
-
-        };
-    }
+//
+//            HashMap<String, AttributeValue> key_to_get =
+//                    new HashMap<String, AttributeValue>();
+//
+//            key_to_get.put("name", AttributeValue.builder()
+//                    .s("name").build());
+//
+//            DeleteItemRequest request = DeleteItemRequest.builder()
+//                    .key(key_to_get)
+//                    .tableName("mateusserejo_github")
+//                    .build();
+//
+//            DynamoDbClient ddb = DynamoDbClient.create();
+//            ddb.deleteItem(request);
+//        }
+//    }
 }
